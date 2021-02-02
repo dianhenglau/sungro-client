ef584855b898d9611cf2cff06c292d9272e6b63c
dianhenglau <dianhenglau@gmail.com>
2021-02-02 22:01:08 +0800

    Add code to try connecting to server

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,6 +5,16 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import sungro.api.ParamForGetManyUsers;
+import sungro.api.Repo;
+import sungro.api.ResultForGetManyUsers;
+import sungro.api.User;
+
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.ArrayList;
 
 public class Main extends Application {
 
@@ -17,7 +27,20 @@ public class Main extends Application {
     }
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws RemoteException, NotBoundException {
+        Registry registry = LocateRegistry.getRegistry(1099);
+        Repo repo = (Repo) registry.lookup("Repo");
+
+        ParamForGetManyUsers param = new ParamForGetManyUsers();
+        ResultForGetManyUsers result = repo.getManyUsers(param);
+        System.out.println(result.getStatus());
+
+        ArrayList<User> users = result.getUsers();
+        System.out.println(users.size());
+        for (User user : users) {
+            System.out.println(user.getFirstName());
+        }
+
         launch(args);
     }
 }
